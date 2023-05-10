4e79d5933c51886bb0b6c44a149db51b338c415d
Ash Reid <ashley.reid013@gmail.com>
2023-05-09 21:24:29 -0600

    adds delete function

--- routes/notes.js
+++ routes/notes.js
@@ -1,17 +1,42 @@
 const notes = require('express').Router();
-const { readAndAppend, readFromFile } = require('../helpers/rwUtils');
+const { v4: uuidv4 } = require('uuid');
+const { readAndAppend, readFromFile, writeToFile } = require('../helpers/rwUtils');
 
 notes.get('/', (req, res) => 
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
 
+notes.get('/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            const result = json.filter((note) => note.id === noteId);
+            return result.length > 0
+            ? res.json(result)
+            : res.json('No note with that ID');
+        });
+});
+
+notes.delete('/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        const result = json.filter((note) => note.id !== noteId);
+        writeToFile('./db/db.json', result);
+        res.json(`Deleted ${noteId} 🗑️`);
+      });
+});
+
 notes.post('/', (req, res) => {
-    const { title, text } = req.body;
+    const { title, text} = req.body;
 
     if (title && text) {
         const newNote = {
             title,
-            text
+            text,
+            id: uuidv4(),
         };
 
         readAndAppend(newNote, './db/db.json');
